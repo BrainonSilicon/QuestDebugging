400098d1fd7020f8412b89dd1f805af82b78dced
PeruDayani <perudayani@berkeley.edu>
2019-11-09 13:23:28 -0700

    basic logic

---
+++ Assets/Follower.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public GameObject leader;
+    public bool following;
+    public float scale = 0.005f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void AssignLeader(GameObject lead)
+    {
+        leader = lead;
+        following = true;
+    }
+
+    public void UnAssign()
+    {
+        leader = null;
+        following = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (following)
+        {
+            Vector3 target = leader.transform.position;
+            Vector3 direction = target - this.transform.position;
+            Vector3 unitDirection = direction.normalized;
+            Vector3 step = unitDirection * scale;
+            this.GetComponent<Rigidbody>().velocity = step;
+
+            //this.transform.position += step;
+        }
+    }
+}
---
+++ Assets/Leader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leader : MonoBehaviour
+{
+
+    public Follower[] allFollowers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+
+        if(other.gameObject.tag == "Player")
+        {
+
+            this.transform.SetParent(other.gameObject.transform);
+
+            foreach (Follower orb in allFollowers)
+            {
+                orb.AssignLeader(this.gameObject);
+            }
+        }
+
+        if(other.gameObject.tag == "Orb")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
